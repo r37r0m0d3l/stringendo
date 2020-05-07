1f4179b1d94bbfd1ecf5dc2b501aec31ae15dcd0
Anton Trofimenko <r37r0m0d3l@users.noreply.github.com>
2020-05-07 23:18:38 +0300

    Dependencies updated.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,44 +1,4 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import rollupPluginTerser from "rollup-plugin-terser";
+/*  eslint-env node */
+import rollupConfigPkg from "@r37r0m0d3l/rollup-config-pkg";
 
-const INPUT_NAME = "index.mjs";
-const OUTPUT_NAME = "stringendo";
-const UMD_NAME = "stringendo";
-
-export default {
-  input: `./src/${INPUT_NAME}`,
-  output: [
-    {
-      file: `./dist/${OUTPUT_NAME}.cjs`,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: `./dist/${OUTPUT_NAME}.mjs`,
-      format: "es",
-      sourcemap: true,
-    },
-    {
-      file: `./dist/${OUTPUT_NAME}.js`,
-      format: "umd",
-      globals: {},
-      name: UMD_NAME,
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    rollupPluginTerser.terser({
-      keep_classnames: true,
-      keep_fnames: true,
-      output: {
-        comments: false,
-      },
-      sourcemap: true,
-      warnings: true,
-    }),
-  ],
-  external: [],
-};
+export default rollupConfigPkg("stringendo", "stringendo");
